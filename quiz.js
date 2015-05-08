45e2d0083be4fbc30ca569e2c4ad64d3f8b1ba7a
mimorta <gtuk@icloud.com>
2015-05-08 18:30:55 +0300

    ss

--- quiz.js
+++ quiz.js
@@ -2,42 +2,22 @@ var questions = document.getElementsByTagName("question");
 var pos = 0;
 
 /*create Multiple choice question*/
-<<<<<<< HEAD
-=======
-function createMultiple(id){
-    for (var i = 0; i< questions[id].getElementsByTagName("choice").length; i++){
-        questions[id].getElementsByTagName("choice")[i].innerHTML = "<input type='radio' name='choices' value='a" + i + "'>"+questions[id].getElementsByTagName("choice")[i].textContent+"</br>";  
-    } 
-    questions[id].innerHTML += "<button onclick='checkAnswer()'>Перевірити</button></br>";
-}
->>>>>>> origin/master
 
-function createCheckBox(id){
-   for (var i = 0; i< questions[id].getElementsByTagName("choice").length; i++){
-        questions[id].getElementsByTagName("choice")[i].innerHTML = "<input type='checkbox' name='choices' value='a" + i + "'>"+questions[id].getElementsByTagName("choice")[i].textContent+"</br>"; 
+/*function createCheckBox(id){
+    
 }
-	questions[id].innerHTML += "<button onclick='checkAnswer()'>Перевірити</button></br>";
-/*function createNumInPut(id){
+
+function createNumInPut(id){
 }
 
 function createStrInput(id){
 }*/
 
-<<<<<<< HEAD
 function findCorrect() {
     var answers = questions[pos].getElementsByTagName("choice");
     for (i = 0; i < answers.length; i++){
         if (answers[i].getElementsByTagName("input")[0].checked == true && answers[i].getAttribute("correct") == "true" ) {
            document.getElementById("demo").innerHTML = "Hello World";
-=======
-
-function findCorrect(id){
-    var answers = questions[id].getElementsByTagName("choice");
-    var correctAns = 0;
-    for (var i = 0; i < answers.lenght; i++){
-        if (answers[i].getAttribute("correct")== "true"){
-            correctAns = i;
->>>>>>> origin/master
         }
         else if (answers[i].getElementsByTagName("input")[0].checked == true && answers[i].getAttribute("correct") == "false") {
             document.getElementById("demo").innerHTML = "Nahui idi";
